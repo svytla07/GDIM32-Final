0dfe85a6fbd55e52b76189e8d1fb98f941f532a4
svytla07 <vytlas@uci.edu>
2026-03-10 19:12:02 -0700

    Attempt to Fix Mouse Movement

--- GDIM32 Final/Assets/Scripts/Player.cs
+++ GDIM32 Final/Assets/Scripts/Player.cs
@@ -21,5 +21,28 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.Translate(Vector3.forward * v * moveSpeed * Time.deltaTime);
         }
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            movement.x = -1;
+        }
+
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            movement.x = 1;
+        }
+
+        transform.Translate(movement * _moveSpeed * Time.deltaTime);
+
+        // float rotation = Input.GetAxis("Horizontal") * _turnSpeed * Time.deltaTime;
+        // transform.Rotate(0, rotation, 0);
+
+        // transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * _mouseSpeed, Input.GetAxis("Mouse X") * _mouseSpeed, 0));
+=======
+>>>>>>> e3b2960df768d13202395f2cd6f3179de876d419
+>>>>>>> Stashed changes
     }
 }
