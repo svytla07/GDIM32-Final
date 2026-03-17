5e0755b3166ececbfb1c838f16404ba157352e81
svytla07 <vytlas@uci.edu>
2026-03-16 21:19:49 -0700

    NPC Quest Stuff

--- GDIM32 Final/Assets/Scripts/Manager.cs
+++ GDIM32 Final/Assets/Scripts/Manager.cs
@@ -5,45 +5,24 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public enum Ingredients
-    {
-        Noodles, Beef, Chicken, Onion, Water, Sugar, Salt
-    }
+  
+    public void Talk() {
+        
+        Quest gather = QuestManager.Instance.gatherIngredients;
+        Quest cookChicken = QuestManager.Instance.cookChickenPho;
+        Quest cookBeef = QuestManager.Instance.cookBeefPho;
 
-    [SerializeField] public List<Ingredients> order = new List<Ingredients>();
-
-
-
-    private Ingredients _order(int randomizedIngredients)
-    {
-        switch (randomizedIngredients)
-        {
-            default: return Ingredients.Beef;
-            case 1: return Ingredients.Noodles;
-            case 2: return Ingredients.Salt;
-            case 3: return Ingredients.Onion;
-            case 4: return Ingredients.Water;
-            case 5: return Ingredients.Sugar;
-            case 6: return Ingredients.Chicken;
+        if (gather.state == QuestState.NotStarted) {
+            gather.state = QuestState.InProgress;
         }
-    }
-
-
-    void Start()
-    {
-        int randomizedIngredients = Random.Range(-1,1);
-        for (int i = 0; i < 6; i++)
-        {
-            
-           randomizedIngredients = randomizedIngredients + 1; 
-           order.Add(_order(randomizedIngredients));
-
+        if (gather.state == QuestState.Completed && cookChicken.state == QuestState.NotStarted) {
+            cookChicken.state = QuestState.InProgress;
         }
-
-
+        if (gather.state == QuestState.Completed && cookChicken.state == QuestState.Complete && cookBeef.state == QuestState.NotStarted) {
+            cookBeef.state = QuestState.InProgress;
+        }
+    
     }
-
  
     
    
