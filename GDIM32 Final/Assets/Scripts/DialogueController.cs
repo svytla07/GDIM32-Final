0cada372d6f48ba88017b97bc9ee081c947cafb1
svytla07 <vytlas@uci.edu>
2026-03-18 20:20:50 -0700

    Choices UI Progress

--- GDIM32 Final/Assets/Scripts/DialogueController.cs
+++ GDIM32 Final/Assets/Scripts/DialogueController.cs
@@ -9,6 +9,8 @@ public class DialogueController : MonoBehaviour
     
     public GameObject dialoguePanel;
     public TMP_Text dialogueText, nameText;
+    public Transform choiceContainer;
+    public GameObject choiceButtonPrefab;
 
     void Awake()
     {
@@ -28,6 +30,16 @@ public class DialogueController : MonoBehaviour
         dialogueText.text = text;
     }
 
+    public void ClearChoices() {
+        foreach (Transform child in choiceContainer) Destroy(child.gameObject);
+    }
+
+    public GameObject CreateChoiceButton(string choiceText, UnityEngine.Events.UnityAction onClick) {
+        GameObject choiceButton = Instantiate(choiceButtonPrefab, choiceContainer);
+        choiceButton.GetComponentInChildren<TMP_Text>().text = choiceText;
+        choiceButton.GetComponent<Button>().onClick.AddListener(onClick);
+        return choiceButton;
+    }
 
    
    
